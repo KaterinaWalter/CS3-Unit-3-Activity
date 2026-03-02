3d40c205c88d9644527a9bade34604dce94eaba7
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-03-02 17:39:15 +0000

    set up file structure

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+# Create instance of Flask
+app = Flask(__name__)
+
+# Homepage route
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# RUN THE APP (or type flask run in terminal)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
