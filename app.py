83c2bfae4c36942b3f7bccdfab881d24ebc1ebca
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-03-04 13:50:01 +0000

    updated data.json

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def index():
     trainer_name = 'Katerina'
     # Pull data from a JSON file
     with open('static/data.json', 'r') as file:
-        data_dictionary = json.load(file)
+        data_dict = json.load(file)
     # Render index page & pass in data
-    return render_template('index.html', trainer=trainer_name, data=data_dictionary)
+    return render_template('index.html', trainer=trainer_name, data=data_dict)
 
 # RUN THE APP (or type flask run in terminal)
 if __name__ == "__main__":
