089e5cc5d7aaadeaf967ae74eb7b9f2b3678de6b
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-03-04 14:40:17 +0000

    edited dropdown

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 # Create instance of Flask
 app = Flask(__name__)
 
-# Homepage route
+# Homepage route 
 @app.route("/")
 def index():
     # Use Python variables to hold data
@@ -12,17 +12,19 @@ def index():
     # Render index page & pass in data
     return render_template('index.html', generation=pokemon_generation)
 
-# Function to handle form submission
-# the app.route decorate maps to the submit button
+# Results page route (after submitting form)
 @app.route("/submit", methods=['POST'])
 def submit():
     # Get data from HTML form input
     selected_type = request.form.get('type')
-    # Read data from a JSON file
+    # OPTIONAL: Read data from a JSON file
     with open('static/data.json', 'r') as file:
         data_dict = json.load(file)
     # Pass data into results page
     return render_template("results.html", type=selected_type, data=data_dict)
+
+
+
 
 # RUN THE APP (or type flask run in terminal)
 if __name__ == "__main__":
